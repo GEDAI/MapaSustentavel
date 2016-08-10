aca1976a112fdc7fcf25ca56a53cc591530e0a70
ricardowerlang <ricardowerlang@gmail.com>
2016-08-10 17:22:12 -0300

    Integração com Web Service.

---
+++ app/src/main/java/br/edu/faifaculdades/mapasustentavel/app/AppConfig.java
@@ -0,0 +1,9 @@
+package br.edu.faifaculdades.mapasustentavel.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://177.125.244.18:8090/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://177.125.244.18:8090/android_login_api/register.php";
+}
